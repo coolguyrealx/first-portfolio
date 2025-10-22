6779d19041934ec4855a154250304ffbf93604f4
coolguyrealx <28goimajetg@gmail.com>
2025-10-22 13:53:17 -0600

    Add springy emoji cursor effect on window load

---
+++ script.js
@@ -0,0 +1,5 @@
+import { springyEmojiCursor } from "https://unpkg.com/cursor-effects@latest/dist/esm.js";
+window.addEventListener("load", (event) => {
+  new cursoreffects.springyEmojiCursor({ emoji: "🤑" )};
+});
+// Credit to tholman at https://github.com/tholman/cursor-effects //
