f334b5cae571560fcef7906ea9decc1a3ed31bc2
coolguyrealx <28goimajetg@gmail.com>
2025-10-24 09:26:48 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+import { fairyDustCursor } from "https://unpkg.com/cursor-effects@latest/dist/esm.js";
+new fairyDustCursor();
+window.addEventListener("load", (event) => {
+  new cursoreffects.ghostCursor();
+});
